6cd1f21f80792f711cc769fa12d3e9cfaa256c02
riscie <matthias@langaust.com>
2016-01-04 14:51:00 +0100

    refactoring

--- main.go
+++ main.go
@@ -4,40 +4,82 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/olekukonko/tablewriter"
 	"log"
 	"os"
-	"github.com/olekukonko/tablewriter"
-	_ "github.com/denisenkom/go-mssqldb"
+	"strconv"
 )
 
+// the dsn (parameters to connect to the TOPdesk DB)
 const dataSourceName = "SERVER=topdesk;DATABASE=TOPDESK_PROD;integrated security=true"
-
+// the db object needed to query the database
 var db *sql.DB
+//  max length of string inside a colloumn, so that that there are no linebreaks within the table output
+const rowMaxLenght = 23
 
 func main() {
-
-	var input = flag.String("h", "", "search a device via hostname")
+	// Command line flags parsing
+	flag.Usage = usage
 	flag.Parse()
+	var searchString string
+	if flag.NArg() != 1 {
+		//usage()
+		//os.Exit(1)
+		searchString = "nb276" // TODO: Temove this. just for testing purposes
+	} else {
+		searchString = flag.Arg(0)
+	}
 
+	// DB Initialization
 	db = initializeDB()
 	defer db.Close()
 
-	var data = getObjectByHostname(*input)
+	// Checking database connectivity first
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("Error: No connection to the database")
+		os.Exit(1)
+	}
 
+	// TODO: Adding the searchString Parser here
+	// We are only getting items where we find the object-name atm...
+	var results = findByInventoryName(searchString)
+
+	// Table-Forming and output of the result
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"HOSTNAME", "USER", "TYPE", "SPECIFICATION"})
+	table.SetHeader([]string{"#", "HOSTNAME", "USER", "TYPE", "SPECIFICATION"})
 
-	for _, v := range data {
-		table.Append(v)
+	for i, r := range results {
+		//Adding a number to each line
+		line := []string{strconv.Itoa(i+1)}
+		//merging the whole line together and adding it to the table
+		line = append(line, r...)
+		table.Append(line)
 	}
 	table.Render()
 }
 
-func getObjectByHostname(input string) [][]string {
+
+// Displays the help message to the user
+func usage() {
+	fmt.Fprintf(os.Stderr, `
+		Usage: 	topd <search>
+
+		Examples:
+			topd nb2737 		//search by inventory number
+			topd 10.197.10.200	//search by ip
+			topd 01-00-5e-7f-ff-fa	//search by mac
+			topd Bucher		//search by the users name
+	`)
+}
+
+// Finds results by inventory name
+func findByInventoryName(input string) [][]string {
 	var data [][]string
 	var naam, ref_gebruiker, objecttype, specificatie string
 
-	rows, err := db.Query("select naam, ref_gebruiker, objecttype, specificatie FROM hardware where naam Like '%"+input+"%'")
+	rows, err := db.Query("select naam, ref_gebruiker, objecttype, specificatie FROM hardware where naam Like '%" + input + "%'")
 	if err != nil {
 		fmt.Println("Query Error1")
 		log.Fatal(err)
@@ -50,8 +92,12 @@ func getObjectByHostname(input string) [][]string {
 			fmt.Println("Query Error2")
 		}
 
+		// reformating strings
+		naam = shortenIfStringLongerThan(naam, rowMaxLenght)
+		ref_gebruiker = shortenIfStringLongerThan(ref_gebruiker, rowMaxLenght)
+		objecttype = shortenIfStringLongerThan(objecttype, rowMaxLenght)
+		specificatie = shortenIfStringLongerThan(specificatie, rowMaxLenght)
 
-		specificatie = shortenIfStringLongerThan(specificatie,23)
 		row := []string{naam, ref_gebruiker, objecttype, specificatie}
 		data = append(data, row)
 
@@ -64,24 +110,26 @@ func getObjectByHostname(input string) [][]string {
 	return data
 }
 
+// initializes the database connection
 func initializeDB() *sql.DB {
 	db, err := sql.Open("mssql", dataSourceName)
 	if err != nil {
 		fmt.Println("DB initialization Error")
 		log.Fatal(err)
 	}
-
 	return db
 }
 
-func shortenIfStringLongerThan(s string, maxLength int) string{
+// utlilty-function to help shorten strings so that there
+// are no linebreaks within the table output
+func shortenIfStringLongerThan(s string, maxLength int) string {
 	a := []rune(s)
 	var returnValue string
-	if len(a) > maxLength{
-		for i:=0;i<maxLength;i++{
-			returnValue+=string(a[i])
+	if len(a) > maxLength {
+		for i := 0; i < maxLength; i++ {
+			returnValue += string(a[i])
 		}
-		returnValue+="..."
+		returnValue += "..."
 	}
 	return returnValue
 }
